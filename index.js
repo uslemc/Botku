122ebcea5a77794e9735e8b314bafa17c22c840e
uslemc <61359213+uslemc@users.noreply.github.com>
2020-02-23 00:40:48 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,41 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+
+const PREFIX = 'rpg';
+
+bot.on('ready', () =>{
+    console.log('Bot nya onlen kok');
+})
+
+bot.on('message', message=>{
+    let args = message.content.substring(PREFIX.length).split(" ");
+    switch(args[1]){
+        case 'hunt':
+            setTimeout(function(){
+               message.reply('ya hunt lagi')
+            },1*60000);
+        break;
+
+        case 'ladder':
+            setTimeout(function(){
+                message.reply('ya ladder lagi')
+            },1*300000);
+            break;
+        
+        case 'axe':
+            setTimeout(function(){
+                message.reply('ya axe lagi')
+            },1*300000);
+            break;
+
+        case 'net':
+            setTimeout(function(){
+                message.reply('ya net lagi')
+            },1*300000);
+            break;
+    }
+
+})
+
+bot.login(process.env.BOT_TOKEN);
